402d8bd7b1eeaef371b11b851649d7323f48e6de
mghendi <sam.mwamburi@gmail.com>
2019-03-28 14:59:37 +0300

    Initial commit
    Created and linked basic dependencies.

---
+++ assets/php/_js.php
@@ -0,0 +1,3 @@
+<script src="assets/js/jquery-3.3.1/jquery-3.3.1.min.js"></script>
+<script src="assets/js/bootstrap-4.2.1-dist/bootstrap.min.js"></script>
+<script src="assets/js/fontawesome-free-5.7.2-web/fontawesome.min.min.js"></script>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home | Site</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include 'assets/php/_css.php' ?>
+</head>
+<body>
+    
+    <?php include 'assets/php/_js.php'?>
+</body>
+</html>
