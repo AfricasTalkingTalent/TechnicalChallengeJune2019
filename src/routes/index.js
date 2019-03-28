e21fd170f0442ccf5066e5f3f74ab69d9179c1a3
omamonaphtali <omamonaphtali@gmail.com>
2019-03-29 02:48:31 +0300

    cleaned the directory

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 
-const account = require('../accounts');
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  // res.render('index', res.locals.commonData);
-  console.log("router js works");
+app.get('/', (req, res) => {
+  // res.render('ussd', res.locals.commonData);
+  res.render('ussd');
 });
 
 module.exports = router;
--- ussd.js
+++
@@ -1,9 +0,0 @@
-if (text == ' '){
-  response  = "CON Enter your username \n"
-  username = response;
-}
-else if (text == '*'){
-  response = "CON Enter your email \n"
-  email = response;
-}
-    
