eab4c2ab40626d3cf09010d2b4a8887bfeb32505
Kimaru Thagana <thagana44@gmail.com>
2019-03-28 18:42:04 -0300

    comments on README

--- task2/task2.py
+++ task2/task2.py
@@ -10,12 +10,12 @@ username =  "sandbox"  #  alternatively use environment variables
 api_key =  "d74e4822dc6e1d8628d11b129e2f8d42d01d5a4b82ba4475f0ea229bf169db67"
 africastalking.initialize(username, api_key)
 
-# Initialize service e.g. SMS
+# Initialize service--> SMS
 sms = africastalking.SMS
 
 def successmessage(username, email):
     username=username.split('*')[-1] # split by asterisk and take last item this will be the username
     email=email.split('*')[-1]# split by asterisk and take last item this will be the email
     # communicate to user
-    response = sms.send("Successful account registration\n Your are receiving this message because you have signed up for an account at \n"+
-    "JUNETALK Below are your details \n username:"+str(username)+"\n Email:"+str(email)+"\n Visit https://www.juntalk2019.com for more information", ["+254726782953"])
+    sms.send("Successful account registration\n Your are receiving this message because you have signed up for an account at \n"+
+    "JUNETALK Below are your details \n username:"+str(username)+"\n Email:"+str(email)+"\n Visit https://www.junetalk2019.com for more information", ["+254726782953"])
