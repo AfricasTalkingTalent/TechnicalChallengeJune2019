08ff9ed6de494b7062f3ef5ff21bdd29d32d744b
Barak <bachillah@gmail.com>
2019-03-28 22:40:33 +0300

    finished task 1

---
+++ task_1.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+import glob
+
+#argument parser for capturing the dirpath for searching for documents. in command line you'll specify the dirpath as argument after the -f
+ap=argparse.ArgumentParser()
+ap.add_argument('-f','--dirpath',required=True,
+                help="Directory to search for documents")
+args=vars(ap.parse_args())
+dirpath=args['dirpath']
+
+#funciton for finding character 'a' inside document X
+def find_a(X):
+    with open(X,'r') as f:
+        for line in f:
+            if 'a' in line:
+                return True
+    return False
+
+
+files=glob.glob(dirpath+"/*.*")     #capturing all documents that follow format *.*
+files=[file for file in files if not os.path.isdir(file)]   #excluding directories which may be named with a '.' and be confused as a document
+results=list(map(find_a,files)) #mapping the find_a function to the list of files in the directory
+
+#creates a list of documents whose index number in the results file have bool True
+docs=[file for file in files if results[(files.index(file))] is True]
+
+if docs==[]:
+    print("No documents found containing 'a'. Did you input a valid file extension?")
+else:
+    for doc in docs:
+        print(doc)
