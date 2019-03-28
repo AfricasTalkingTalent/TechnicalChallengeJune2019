8517bd7d2a1a6bf106b5ceb5a1333829caa7438e
Kimaru Thagana <thagana44@gmail.com>
2019-03-28 12:54:58 -0300

    initialize sdk

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,11 @@
 import africastalking
+
+# Initialize SDK
+username =  "sandbox"  #  alternatively use environment variables
+api_key =  "d74e4822dc6e1d8628d11b129e2f8d42d01d5a4b82ba4475f0ea229bf169db67"
+africastalking.initialize(username, api_key)
+
+# Initialize service e.g. SMS
+sms = africastalking.SMS
+response = sms.send("Hello Message!", ["+254726782953"])
+print(response)
