f3783b09a3fe80a6342bc589111f76fb2c77ef1e
VickyMutai <vicky.mutai96@gmail.com>
2019-03-29 07:08:22 +0300

    output files with letter a

--- Task1/task1.py
+++ Task1/task1.py
@@ -1,9 +1,30 @@
-# reading all a in a single file
-searchFile = open( "myFile.txt", "r" )
-# loop through all lines in the file given
-for line in searchFile:
-    #check if a is in line
-    if 'a' in line:
-        print (line);
-#closes the file
+"""
+A function that checks and returns all files with letter "a"
+
+Will work with the any directory
+"""
+import os
+#link to the document
+basepath = "../Task1"
+file = []
+files = []
+#checks for files and directory in the path
+for entry in os.listdir(basepath):
+    #outputs only the files
+    if os.path.isfile(os.path.join(basepath, entry)):
+        #reads all files
+        searchFile = open( entry , "r" );
+        #loop through the file line by line
+        for line in searchFile:
+            #checks if "a" is in line
+            if 'a' in line:
+                #appends all entries to a file
+                file.append(entry);
+                #loops through all entries to ensure no duplicates
+                for name in file:
+                    if name not in files:
+                        files.append(name);
+#outputs as a list
+print(files);
+#closes the opened files
 searchFile.close();
