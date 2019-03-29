c23f091275405c40d7f54bf63a2d6ab5aec7609f
EverlynAsiko <asikoeverlyn@gmail.com>
2019-03-29 06:04:32 +0300

    Added notes on read me + ussd and sms codes

--- task 2/ussd.php
+++ task 2/ussd.php
@@ -1,6 +1,8 @@
-
 <?php
-function get_information(){
+
+//Information below is to host the USSD bit
+//Created a db to help me store information
+/* function get_information(){
     $host="dannextech.com";
     $user="dannexte";
     $pass="*******";
@@ -15,42 +17,38 @@ function get_information(){
     
     $sql = "SELECT DISTINCT * FROM info";
     $result = $conn->query($sql);
-    
-    
-    
-    
+
             if ($result->num_rows > 0) {
                 return $result;
             } else {
                 return "nothing";
             }
             $conn->close();
-}
+}*/
 	  
-// Reads the variables sent via POST from our gateway
-	$sessionId   = $_POST["sessionId"];
-	$serviceCode = $_POST["serviceCode"];
-	$phoneNumber = $_POST["phoneNumber"];
-	$text        = $_POST["text"];
-	if ( $text == "" ) {
-	     // This is the first request. Note how we start the response with CON
-	     $response  = "CON Welcome to USSD registration! Please enter your username:\n";
-	}
-	else if ( $text == "" ) {
-	  // Business logic for first level response
-	  $response = "CON Username successfully registered. Please enter email address: \n"; 
-	  
-	 }
-	 else if($text == "1*1") {
-	  // This is a second level response where the user selected 1 in the first instance
-	  //$accountNumber  = "ACC1001";
-	  // This is a terminal request. Note how we start the response with END
-	  $result = "Thank you for registering. Confirmation message coming soon."
-	  
-	 }
-	//
-	// Print the response onto the page so that our gateway can read it
-	header('Content-type: text/plain');
-	echo $response;
-	// DONE!!!
+    // Reads the variables sent via POST from our gateway
+    $sessionId   = $_POST["sessionId"];
+    $serviceCode = $_POST["serviceCode"];
+    $phoneNumber = $_POST["phoneNumber"];
+    $text        = $_POST["text"];
+    if ( $text == "" ) {
+        // This is the first request. Note how we start the response with CON
+        $response  = "CON Welcome to USSD registration! Please enter your username:\n";
+    }
+    else if ( $text != "" ) {
+        // Second menu
+    	$response = "CON Username successfully registered. Please enter email address: \n"; 
+    	  
+    }
+    else if($text != "") {
+    	// This is a second level response where the user selected 1 in the first instance
+    	// This is a terminal request. Note how we start the response with END
+    	$result = "END Thank you for registering. Confirmation message coming soon."
+    	  
+    }
+
+    // Print the response onto the page so that our gateway can read it
+    header('Content-type: text/plain');
+    echo $response;
+    // DONE!!!
 ?>
