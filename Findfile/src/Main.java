8d8a2c56270e19ff8b20dad6edd7361887df4373
Drake <drake.kufwafwa@gmail.com>
2019-03-29 01:42:39 -0400

    Added comments

--- Findfile/src/Main.java
+++ Findfile/src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
+	/*
+	 * public static void main(String[] args)
+	 * args -> takes in a string of arguments from the commandline when running the program.
+	 * Be sure to provide a string of filenames as arguments separated by spaces.
+	*/
 	public static void main(String[] args) {
+		
+		//Creates an instance of the File finding class
 		FindFile findFile = new FindFile();
+		
+		//Calls the foundFile method inside the findFile method which returns an array of all found files
 		String[] found = findFile.foundFile(args);
-		for(int i = 0; i < found.length; i++) {
-			String s = found[i];
-			if(s != null)
+		
+		//Prints all documents where 'a' was found
+		for(String s : found)
+			if(s!= null)
 				System.out.println(s);
-		}
 	}
 }
