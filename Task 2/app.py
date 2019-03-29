f32ddfbf903d62a0bf01b5600ab3ccc309a07dd1
ogaroh <ogaroerick1998@gmail.com>
2019-03-29 03:34:24 +0300

    task 1: document listing

---
+++ Task 2/app.py
@@ -0,0 +1,15 @@
+#  Erick Ogaro, 2019.
+
+import africastalking
+
+username = "sandbox"
+apikey = "112f3eb08f553c2bd555283a72aed140c323109498f5bfbc5c2e2e1c39337f5d"
+africastalking.initialize(username, apikey)
+
+# initialize service
+sms = africastalking.SMS
+ussd = africastalking.USSD
+
+# use service synchronously
+response = sms.send("Hello Erick!", ["+254792835893"])
+print(response)
