b0d0329a9cb8d11c81c74b7d1cc055453fa747fa
Achie Gebre <achiegebre@nat-oitwireless-inside-vapornet100-10-9-192-66.princeton.edu>
2019-03-29 00:53:46 -0400

    added Task1

---
+++ AT.py
@@ -0,0 +1,7 @@
+#define a method finddocs that reads and applies the lambda function to each value in the list.
+
+def finddocs(wfiles):
+	f = open(x, 'r')
+    return list(filter(lambda x:'a' in f.read(), wfiles))
+
+my_function(['wdocuments','hw1_test.dat'])
