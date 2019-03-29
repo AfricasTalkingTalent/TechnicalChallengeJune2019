f4779cd9b5cf5ed44e77859dd9cbce43172f2ae1
VickyMutai <vicky.mutai96@gmail.com>
2019-03-29 08:59:06 +0300

    check if ussd works

--- Task2/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-# Initializing application
-app = Flask(__name__)
-
-from app import views
--- Task2/ussd.py
+++ Task2/ussd.py
@@ -1,2 +1,38 @@
-from flask import Flask
+import os
+from flask import Flask, request
+
 app = Flask(__name__)
+
+response = ""
+
+@app.route('/', methods = ['GET','POST'])
+
+def ussd():
+    global response
+    session_id = request.values.get("sessionId",None)
+    servicecode =  request.values.get("serviceCode",None)
+    phone_number = request.values.get("phoneNumber",None)
+    email =  request.values.get("email",None)
+    name = request.values.get("name",None)
+    text =  request.values.get("text",None)
+
+    response = "Whats your name: "
+
+
+    if text == "":
+        response = "Fill the details \n"
+        response += "1. Name\n"
+        response += "2. My Email\n"
+    elif text == "1":
+        response = "Write your name:\n"
+        name += " \n"
+    elif text == "2":
+        response = "Write your email:\n"
+        email += " \n"
+    else:
+        response = "END Invalid choice"
+
+        return response
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=os.environ.get("PORT"),debug=True)
