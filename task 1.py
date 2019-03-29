0926bddfbfaa25cba2d87cdd1430083fb0288bd1
EverlynAsiko <asikoeverlyn@gmail.com>
2019-03-29 03:28:13 +0300

    Added task 1.py file

---
+++ task 1.py
@@ -0,0 +1,4 @@
+#To do:
+# 1. Find words in a document containing letter 'a'
+# 2. Create a loop that can find words with a in multiple document
+# 3. Create a list of documents that contain words with a
